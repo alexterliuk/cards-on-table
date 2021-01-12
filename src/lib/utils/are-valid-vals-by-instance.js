f207c3624d3f6f2ad5ea85c69b9f7d7d2bb37439
Alex Terliuk <alex.terliuk@gmail.com>
2021-01-12 18:42:30 +0200

    feat: add areValidValsByInstance util function

---
+++ src/lib/utils/are-valid-vals-by-instance.js
@@ -0,0 +1,19 @@
+import { isArray, isFunction } from './value-checkers';
+
+/**
+ * @param {array} arr
+ * @param {function} klass - Class to check whether val is instance of it
+ * @param {boolean} allowEmptyArr - should empty arr be evaluated to true
+ * @returns {undefined|boolean}
+ */
+const areValidValsByInstance = (arr, klass, allowEmptyArr) => {
+  if (!isArray(arr) || !isFunction(klass)) return;
+  if (allowEmptyArr && !arr.length) return true;
+  if (arr.length) {
+    return arr.every(c => c instanceof klass);
+  }
+  // in case if arr is empty array
+  return false;
+};
+
+export default areValidValsByInstance;
