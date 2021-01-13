57f37b8ae14d359613c8f708dac723eb4bd3350c
Alex Terliuk <alex.terliuk@gmail.com>
2021-01-14 00:20:24 +0200

    chore: clarify params signature in Suit

--- src/game/inventory/suit.js
+++ src/game/inventory/suit.js
@@ -2,6 +2,11 @@ import Card from './card.js';
 import { isString } from '../../lib/utils/value-checkers.js';
 
 export default class Suit {
+  /**
+   * @param {string} name
+   * @param {array} cardsData - of arrays with cards params ([name, value, rank])
+   *    e.g. for suit with only 2 cards: [['king', 4, 2], ['queen', 3, 1]]
+   */
   constructor(name, cardsData) {
     if (!isString(name)) {
       throw new Error('name must be string when creating new Suit.');
