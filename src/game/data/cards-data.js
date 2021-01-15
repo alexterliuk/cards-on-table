7bac1f43799cef6a34343f8ebd366161f83ba8ec
Alex Terliuk <alex.terliuk@gmail.com>
2021-01-15 20:33:51 +0200

    refactor: suitNames -> defaultSuitNames, add deberts data

--- src/game/data/cards-data.js
+++ src/game/data/cards-data.js
@@ -1,7 +1,25 @@
-export const suitNames = ['spades', 'hearts', 'diamonds', 'clubs'];
+export const defaultSuitNames = ['spades', 'hearts', 'diamonds', 'clubs'];
 
 export const defaultSuitData = [
   // name, value, rank
+  ['ace', 14, 13],
+  ['king', 13, 12],
+  ['queen', 12, 11],
+  ['jack', 11, 10],
+  ['ten', 10, 9],
+  ['nine', 9, 8],
+  ['eight', 8, 7],
+  ['seven', 7, 6],
+  ['six', 6, 5],
+  ['five', 5, 4],
+  ['four', 4, 3],
+  ['three', 3, 2],
+  ['two', 2, 1],
+];
+
+export const defaultTrumpSuitData = defaultSuitData.map(d => d.map(s => s));
+
+export const defaultDebertsSuitData = [
   ['ace', 11, 8],
   ['ten', 10, 7],
   ['king', 4, 6],
@@ -12,8 +30,7 @@ export const defaultSuitData = [
   ['seven', 0, 1],
 ];
 
-export const defaultTrumpSuitData = [
-  // name, value, rank
+export const defaultDebertsTrumpSuitData = [
   ['jack', 20, 8],
   ['nine', 14, 7],
   ['ace', 11, 6],
