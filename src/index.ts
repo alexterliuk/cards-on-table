9ca813a582f5ae1bdaa215c53908042f133a0a37
Alex Terliuk <alex.terliuk@gmail.com>
2023-04-15 14:12:11 +0300

    expose only createTable method from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,32 @@
 import Player from './actors/player';
-import Card from './inventory/card';
 import Deck from './inventory/deck';
-import Suit from './inventory/suit';
 import Table from './inventory/table';
-import deckCardsData from './data/deck-cards-data';
-
-const deck = new Deck(deckCardsData);
-const p1 = new Player(deck);
-const p2 = new Player(deck);
-const p3 = new Player(deck);
-const p4 = new Player(deck);
+import { DeckCardsData } from './data/deck-cards-data';
 
 class CardsOnTable {
-  name: string;
-  description: string;
-  _player: typeof Player;
-  _card: typeof Card;
-  _deck: typeof Deck;
-  _suit: typeof Suit;
-  _table: typeof Table;
-  players: Array<Player>;
-  deck: Deck;
-  table: Table;
+  about: string;
+  createTable: typeof createTable;
 
   constructor() {
-    this.name = 'Cards On Table';
-    this.description =
-      'The library for the construction of playing cards game.';
-    this._player = Player;
-    this._card = Card;
-    this._deck = Deck;
-    this._suit = Suit;
-    this._table = Table;
-    this.players = [p1, p2, p3, p4];
-    this.deck = deck;
-    this.table = new Table(this.deck, this.players);
+    this.about =
+      'Cards On Table - the library for the construction of playing cards game.';
+    this.createTable = createTable;
   }
 }
 
 const cds = new CardsOnTable();
+
+function createTable(deckCardsData: DeckCardsData, playerQty: number) {
+  const deck = new Deck(deckCardsData);
+  const players = Array(playerQty)
+    .fill(null)
+    .reduce(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      (acc: Player[], player: Player) => (acc.push(new Player(deck)), acc),
+      [],
+    );
+
+  return new Table(deck, players);
+}
 
 export default cds;
